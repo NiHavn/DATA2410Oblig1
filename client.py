d032fa74d92ee3bd7868b9f4ef0b3bd97552775e
Niklas Havnaas <Niklas.o.h@hotmail.com>
2023-03-06 21:38:38 +0100

    mindre forbedringer

--- client.py
+++ client.py
@@ -2,17 +2,22 @@
 import sys
 from socket import *
 
+# Making variables to use in commandline
 host = sys.argv[1]
 port = int(sys.argv[2])
 filename = sys.argv[3]
 
+#TCP Client socket
 conSock = socket(AF_INET, SOCK_STREAM)
 
+#Establishing serverconnection with host and port
 conSock.connect((host, port))
 
+#Sending a ET request to the server
 request = "GET /" + filename + " HTTP/1.1\r\nHost: " + host + "r\n\r\n"
 conSock.send(request.encode())
 
+#Recieving response from server
 msg = b''
 while True:
     data = conSock.recv(2048)
@@ -21,8 +26,10 @@ while True:
 
     msg += data
 
+#Printing the message
 print(msg.decode())
 
-
+#Closing socket
 conSock.close()
+#Exiting
 sys.exit()
--- server.py
+++ server.py
@@ -7,47 +7,42 @@ import sys # In order to terminate the program
 
 def handleRequest(conSocket):
     try:
-        message = conSocket.recv(2048).decode() #
-        print(message) 
-        filename = message.split()[1][1:]
-        print(filename)
-        
+        #Recieving the request from the client
+        message = conSocket.recv(2048).decode() 
+        filename = message.split()[1][1:] #the[1][1:] specifies what portion of the file name to send
         f = open(filename)
         outputdata = f.read()
         
-        """
-        with open(filename, 'rb') as sp:
-            outputdata = sp.read()
-        """
         #Send one HTTP header line into socket
-        conSocket.send('HTTP/1.1 200 OK\r\n'.encode())
+        conSocket.send('HTTP/1.1 200 OK\r\n'.encode()) #Statusline
         conSocket.send('Content-Type: text/html\r\n'.encode())
         conSocket.send(('Content-Length: %d\r\n' % len(outputdata)).encode())
-        conSocket.send('\r\n'.encode())
+        conSocket.send('\r\n'.encode())     #Makes the end of the response
+
         #Send the content of the requested file to the client 
         for i in range(0, len(outputdata), 2048):
             conSocket.send((outputdata[i:1+2048]).encode())
-    except IOError:
+        #the for-loop goes sends the characters from 0 to the length of outdata
+    except IOError:  #Used for exceptionhandling, if the server ant find the specified file, it will return an 404 exception
     #Send response message for file not found
-        conSocket.send('HTTP/1.1 404 Not Found\r\n'.encode())
+        conSocket.send('HTTP/1.1 404 Not Found\r\n'.encode())  #404 response if server cant find file
         conSocket.send('Content-Type: text/html\r\n'.encode())
         conSocket.send('\r\n'.encode())
         conSocket.send('<html><body><h1>404 Not Found</h1></body></html>'.encode())
     #Close client socket
     finally:
-        conSocket.close()
+        conSocket.close() #Makes sure the program exits when done
 
-serverSocket = socket(AF_INET, SOCK_STREAM) 
-host = '127.0.0.1' #Establishes a ip for the host
+serverSocket = socket(AF_INET, SOCK_STREAM) #Server socket
+host = '127.0.0.1' #Establishes a host-ip for the host
 port = 6789        #Defines what port communication with server happens
-serverSocket.bind(('', port))
-serverSocket.listen(5)
+serverSocket.bind(('', port))  #
+serverSocket.listen(5) #Max nuber of users at the same time
 
 
 while True:
 #Establish the connection print('Ready to serve...') connectionSocket, addr =
     print('Ready to serve....')
     conSocket, addr = serverSocket.accept()
-
     t = threading.Thread(target=handleRequest, args=(conSocket,))
     t.start()
